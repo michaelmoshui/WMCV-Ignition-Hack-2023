df27169faaa9f42a06bdeae15f3b6ad8d46fa12f
ColineZhang <kexuanzhang04@gmail.com>
2023-08-26 18:32:55 -0400

    amazon_api function updated

--- Backend/amazon_api/product_search.js
+++ Backend/amazon_api/product_search.js
@@ -1,16 +1,33 @@
-
-function product_search(search_keyword, number_of_products=5){
-const amazonScraper = require('amazon-buddy');
-
-(async () => {
+async function product_search(search_keywords, number_of_products = 3) {
+    const amazonScraper = require('amazon-buddy');
+    const results = [];
+  
     try {
-        // Collect 50 products from a keyword 'xbox one'
-        // Default country is US
-        const products = await amazonScraper.products({ keyword: search_keyword, number: number_of_products });
+      for (const keyword of search_keywords) {
+        // Collect products for each keyword in the array
+        const products = await amazonScraper.products({ keyword: keyword, number: number_of_products });
+  
+        // Ensure products is an array and contains items
+        const info = products['result'];
+        if (Array.isArray(info) && info.length > 0) {
+          const firstThreeProducts = info.slice(0, Math.min(info.length, 3));
+          results.push(...firstThreeProducts);
+        }
+      }
+      return results;
     } catch (error) {
-        console.log(error);
+      console.log(error);
+      return null;
     }
-})();
-}
-
-product_search('orange juice');
+  }
+  
+  // Example usage with an array of search keywords
+  const searchKeywords = ['xbox one', 'playstation 5', 'nintendo switch'];
+  product_search(searchKeywords, 3)
+    .then(productInfoList => {
+      console.log(productInfoList);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  
