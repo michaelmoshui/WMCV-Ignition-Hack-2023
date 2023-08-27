4f734e6c9ebeab7b17ef95a0a4cc2917f3eecd3e
Michael Mo <michaelmoshui@gmail.com>
2023-08-27 04:37:14 -0400

    Get most of frontend working

---
+++ frontend/src/Components/ItemDescription/ItemDescription.js
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import useClickOutside from "../../helper/clickOutside";
+import styles from "./ItemDescription.module.css";
+
+export default function ItemDescription({
+  item,
+  showItemDescription,
+  setShowItemDescription,
+  ind,
+  iniBooleans,
+}) {
+  const itemLoc = useRef(null);
+  useClickOutside(itemLoc, () => {
+    setShowItemDescription(iniBooleans);
+  });
+
+  return (
+    <div>
+      {showItemDescription && (
+        <div className={styles.blur}>
+          <div className={styles.itemWrapper} ref={itemLoc}>
+            <div className={styles.title}>{`${item?.title}`}</div>
+            <div className={styles.line}></div>
+            <div className={styles.evaluation}>{`${item?.description}`}</div>
+            <div>{`${ind}`}</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
